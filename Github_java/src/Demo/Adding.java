6e4f6235e913b7c697be97312c371da9820ca384
Muhindhar <muhindharsv27@gmail.com>
2026-02-20 14:25:47 +0530

    Initial commit

---
+++ Github_java/src/Demo/Adding.java
@@ -0,0 +1,13 @@
+package Demo;
+import java.util.*;
+public class Adding {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 10;
+		int b = 20;
+		int c = a+b;
+		System.out.println(c);
+	}
+
+}
